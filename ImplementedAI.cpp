9cd99d73daa4715ce40af9d28cc52eefdc1093fe
katana7755 <jaeho@unity3d.com>
2021-08-20 05:54:45 +0900

    committed the AI which passed Wooden 1 league
    - implemented when it uses boost, and that is dependent on distance, direction and also speed
    - in addition, added a distance condition for thrust

--- ImplementedAI.cpp
+++ ImplementedAI.cpp
@@ -3,8 +3,62 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <chrono>
 
 using namespace std;
+
+/**
+* Utility functions & classes
+**/
+
+class MathHelper
+{
+private:
+    MathHelper() {}
+
+public:
+    // this function assumes t is always greater than or equal to zero
+    static bool isApproximatelyZero(float t)
+    {
+        return t < 0.001f;
+    }
+
+    // this function assumes t is always greater than or equal to zero
+    static bool isApproximatelyOne(float t)
+    {
+        return t > 0.999f;
+    }
+};
+
+class TimeManager
+{
+private:
+    TimeManager() {};
+
+public:
+    static void initialize()
+    {        
+        s_LastCheckedTime = chrono::steady_clock::now(); 
+    }
+
+    static void update()
+    {        
+        auto currentTime = chrono::steady_clock::now();
+        s_DeltaTime = chrono::duration_cast<chrono::milliseconds>(currentTime - s_LastCheckedTime).count() / 1000.0f;
+    }
+
+    static float getDeltaTime()
+    {
+        return s_DeltaTime;
+    }
+
+private:    
+    static chrono::time_point<chrono::steady_clock> s_LastCheckedTime;
+    static float s_DeltaTime;
+};
+
+chrono::time_point<chrono::steady_clock> TimeManager::s_LastCheckedTime;
+float TimeManager::s_DeltaTime;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -13,6 +67,11 @@ using namespace std;
 
 int main()
 {
+    int boostCount = 1;
+    bool isInitialized = false;
+    int lastX, lastY;
+    TimeManager::initialize();
+
     // game loop
     while (1) {
         int x;
@@ -28,15 +87,47 @@ int main()
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
+        cerr << "distance to the next target: " << nextCheckpointDist << endl;
 
-		// Spend thrust propotionally to (1 - t^2) in order to get more power when it become closer to zero
-        float t = min(abs(nextCheckpointAngle) / 90.0f, 1.0f);
-        int thrust = (int)(100.0f * (1 - t * t));
-        
+        TimeManager::update();
+
+        float speed = 0.0f;
+
+        if (isInitialized)
+        {
+            float xx = (x - lastX) * (x - lastX);
+            float yy = (y - lastY) * (y - lastY);
+            speed = sqrt(xx + yy) / TimeManager::getDeltaTime();
+        }
+        else
+        {   
+            isInitialized = true;         
+        }
+
+        cerr << "speed: " << speed << endl;
+
+        lastX = x;
+        lastY = y;
+
+        float dirFactor = clamp(abs(nextCheckpointAngle) / 90.0f, 0.0f, 1.0f);
+        float distFactor = clamp(nextCheckpointDist / max(speed * 0.1f, 0.01f), 0.0f, 1.0f);
+        string strControl;
+
+        // Use boost when the count is at least 1, the orientation is facing the target, and distance is far enough
+        if (boostCount > 0 && MathHelper::isApproximatelyZero(dirFactor) && MathHelper::isApproximatelyOne(distFactor) && speed > 8000.0f)
+        {
+            --boostCount;   
+            strControl = "BOOST";
+        }
+        else 
+        {        
+            int thrust = (int)(80.0f * distFactor) + (int)(20.0f * clamp(1.1f - dirFactor, 0.0f, 1.0f));
+            strControl = to_string(thrust);
+        }
 
         // You have to output the target position
         // followed by the power (0 <= thrust <= 100)
         // i.e.: "x y thrust"
-        cout << nextCheckpointX << " " << nextCheckpointY << " " << thrust << endl;
+        cout << nextCheckpointX << " " << nextCheckpointY << " " << strControl << endl;
     }
 }
